e25ed37be99f0cb606c102f119b45ce4a1184f0d
David Joy <joy@colorado.edu>
2014-03-18 19:40:50 -0400

    Fix sourceforge url

--- gdcm.rb
+++ gdcm.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Gdcm < Formula
   homepage "http://sourceforge.net/projects/gdcm/"
-  url "https://downloads.sourceforge.net/projects/gdcm/files/gdcm%202.x/GDCM%202.4.1/gdcm-2.4.1.tar.bz2"
+  url "https://downloads.sourceforge.net/project/gdcm/gdcm%202.x/GDCM%202.4.1/gdcm-2.4.1.tar.bz2"
   sha1 "1ba1b46455b962d78f0ad4863872ab06019f3c23"
 
   option "with-check", "Run the GDCM test suite"
