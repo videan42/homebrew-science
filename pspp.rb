42c27e9141edb53b66a165b0e6b0645944e1c56f
David Joy <joy@colorado.edu>
2014-11-25 12:07:18 -0500

    Fix failing tests and enable postgres driver
    Ignore stderr warnings from regression in cairo in 10.10.

--- pspp.rb
+++ pspp.rb
@@ -9,6 +9,8 @@ class Pspp < Formula
   option 'without-check', 'Skip running the PSPP test suite'
   option 'without-gui', 'Build without gui support'
 
+  option 'with-postgresql', 'Build with postgres support'
+
   depends_on 'pkg-config' => :build
 
   depends_on :x11
@@ -27,8 +29,15 @@ class Pspp < Formula
     depends_on 'fontconfig'
   end
 
+  if build.with? 'postgresql'
+    depends_on 'postgresql'
+  end
+
+  patch :DATA
+
   def install
-    args = ['--disable-rpath', '--without-libpq']
+    args = ['--disable-rpath']
+    args << '--without-libpq' if build.without? 'postgresql'
     args << '--without-gui' if build.without? 'gui'
 
     system './configure', "--prefix=#{prefix}", *args
@@ -42,3 +51,30 @@ class Pspp < Formula
   end
 end
 
+__END__
+diff --git a/tests/language/stats/frequencies.at b/tests/language/stats/frequencies.at
+index d321e57..90d54f5 100644
+--- a/tests/language/stats/frequencies.at
++++ b/tests/language/stats/frequencies.at
+@@ -180,7 +180,7 @@ frequencies /x
+ ])
+ # Cannot use the CSV driver for this because it does not output charts
+ # at all.
+-AT_CHECK([pspp -O format=pdf frequencies.sps], [0], [ignore])
++AT_CHECK([pspp -O format=pdf frequencies.sps], [0], [ignore], [ignore])
+ AT_CLEANUP
+ 
+ # Tests for a bug which crashed PSPP when the median and a histogram
+diff --git a/tests/language/data-io/get-data-psql.at b/tests/language/data-io/get-data-psql.at
+index 692de09..ea9b222 100644
+--- a/tests/language/data-io/get-data-psql.at
++++ b/tests/language/data-io/get-data-psql.at
+@@ -12,7 +12,7 @@ m4_define([INIT_PSQL],
+    PGHOST="$socket_dir"
+    export PGHOST
+    AT_CHECK([initdb -A trust], [0], [ignore])
+-   AT_CHECK([pg_ctl start -w -o "-k $socket_dir -h ''"], [0], [ignore])
++   AT_CHECK([pg_ctl start -w -o "-k $socket_dir -h 'example.com'"], [0], [ignore])
+    trap 'CLEANUP_PSQL' 0
+    AT_CHECK([createdb -h "$socket_dir" -p $PG_PORT $PG_DBASE],
+       [0], [ignore], [ignore])
